b5a670cdf154e59960b309b074f20f3957c72be0
Fulano de Tal <matheusnmilfontc@gmail.com>
2022-07-20 18:37:31 -0300

    teste backend1

--- working/Front-end/Interface.py
+++ working/Front-end/Interface.py
@@ -1,4 +1,8 @@
 #Importa a biblioteca de funções
+#encoding: utf-8
+# -*- coding: utf-8 -*-
 import Functions as fct
+import teste
 #Executa o programa
 fct.MenuPrincipal()
+teste.escrever_contrato()
