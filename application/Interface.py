9fec33a14ca0a82d0760eb825ec313431b75763d
Fulano de Tal <matheusnmilfontc@gmail.com>
2022-07-21 20:29:18 -0300

    Finalizando

--- application/Interface.py
+++ application/Interface.py
@@ -3,8 +3,16 @@
 # -*- coding: utf-8 -*-
 import Functions as fct
 import teste
+import os
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
 import htmlToPDF as pdf
 #Executa o programa
 fct.MenuPrincipal()
 teste.escrever_contrato()
-pdf.html_to_pdf("application/Contrato.html", "ContratoFinal.pdf")
+if __name__ == "__main__":
+
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(CURRENT_DIR, f"{pdf.arquivo}.html")
+    print(filename)
+
+    pdf.html_to_pdf(filename, f"{pdf.nomeF}.pdf")
